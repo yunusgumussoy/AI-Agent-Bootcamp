5b096e3739b58dee20db46f0a0b60426061cd20f
Yunus GÜMÜŞSOY <34836874+yunusgumussoy@users.noreply.github.com>
2025-10-10 00:55:47 +0300

    Add files via upload

---
+++ 23-LocalAIAgent.py
@@ -0,0 +1,29 @@
+# pip install langchain langchain-ollama langchain-chroma pandas
+
+
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+from vector import retriever
+
+model = OllamaLLM(model="llama3.2")
+
+template = """
+You are an exeprt in answering questions about a pizza restaurant
+
+Here are some relevant reviews: {reviews}
+
+Here is the question to answer: {question}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+while True:
+    print("\n\n-------------------------------")
+    question = input("Ask your question (q to quit): ")
+    print("\n\n")
+    if question == "q":
+        break
+    
+    reviews = retriever.invoke(question)
+    result = chain.invoke({"reviews": reviews, "question": question})
+    print(result)
